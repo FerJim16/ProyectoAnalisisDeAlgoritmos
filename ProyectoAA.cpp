8fbf266e49244181d64f17af3cf470918311ccaf
FerJim16 <fdjf12@hotmail.com>
2022-06-11 00:26:23 -0500

    Más comentarios
    Comentarios en general

--- ProyectoAA.cpp
+++ ProyectoAA.cpp
@@ -6,10 +6,12 @@ Primero definir las prioridades para un gready (como mochila)
 Generar banderas para saber si se trata de casa o 
 establecimiento, en caso de ser establecimiento, 
 saber cuál es y si está disponible
+Banderas:
+(X)Está ocupada ya
+(X)Es casa
+(X)Es establecimiento
 
 */
-
-
 
 
 #include <iostream>
@@ -31,17 +33,16 @@ void MenuOcupaciones()//Imprime el menú de ocupaciones
     printf("\33[0;44m     (1) Estudiante      \n");
     printf("     (2) Ama de casa     \n");
     printf("     (3) Médico          \n");
-    printf("     (4) Trabajador      \n");
-    printf("     (5) Deportista      \33[0;0m\n\n");
+    printf("     (4) Deportista      \33[0;0m\n\n");
 } 
 
 /*
-Prioridades:
-Estudiante:Escuela > Biblioteca > Papeleria > Hospital > Carniceria > Super > Tortilleria > Tienda > Farmacia > Iglesia > Tlapaleria
-Ama de casa:
-Medico:
-Trabajador:
-Deportista:
+
+Prioridades: (usar solo las primeras 3)
+Estudiante:     Escuela > Biblioteca > Papeleria > Hospital > Carniceria > Super > Tortilleria > Tienda > Farmacia > Iglesia > Tlapaleria
+Ama de casa:    Super > Carniceria > Tienda > Tortilleria > Hospital > Farmacia > Biblioteca > Papelería > Escuela > Gimnasio > Nutriologo
+Medico:         Hospital > Farmacia > Super > Biblioteca > Papeleria > Tienda > Tortilleria > Carnicería >  Gimnasio > Nutriologo > Escuela
+Deportista:     Gimnasio > Carniceria > Nutriologo > Super > Tienda > Tortilleria > Hospital > Farmacia > Papeleria > Biblioteca > Escuela 
 
 */
 
@@ -57,20 +58,16 @@ int main()
     switch (opcion)
     {
     case 1:
-        /* code */
+        // Code para el Estudiante
         break;
     case 2:
-        // Code desde super
+        // Code para el Ama de casa
         break;
     case 3:
-        // Code desde el hospital
+        // Code para el Medico
         break;
     case 4:
-        //code desde el ¿?
-        break;
-    case 5:
-        //Code desde el gym
-
+        // Code para el Deportista 
         break;
     
     default:
